398ebbcd6395dc6d258bcbb062a54f253615b3af
Coffee <59876771+Yae-chan@users.noreply.github.com>
2025-02-21 16:34:47 +0700

    update

--- app.py
+++ app.py
@@ -6,31 +6,32 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'perpustakaan'
  
 mysql = MySQL(app)
  
-@app.route('/form')
+@app.route('/register', methods = ['POST', 'GET'])
 def form():
     return render_template('form.html')
  
 @app.route('/login', methods = ['POST', 'GET'])
-def login():
+def registerop():
     if request.method == 'GET':
         return "Login via the login Form"
      
     if request.method == 'POST':
         cursor = mysql.connection.cursor()
-        cursor.execute(''' SELECT MAX(id) FROM users ''')
+        cursor.execute(''' SELECT MAX(id_user) FROM users ''')
         result = cursor.fetchone()
-        id = result[0] + 1 if result[0] else 1
-        email = request.form['email']
+        id_user = result[0] + 1 if result[0] else 1
         username = request.form['username']
         password = request.form['password']
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO users VALUES(%s,%s,%s,%s)''',(id,email,username,password))
+        cursor.execute(''' INSERT INTO users VALUES(%s,%s,%s,%s)''',(id_user,username,password,'user'))
         mysql.connection.commit()
         cursor.close()
-        return f"Done!!"
+        return render_template('login.html')
  
+
+
 app.run(host='localhost', port=5000)
